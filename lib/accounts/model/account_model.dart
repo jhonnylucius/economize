282bad86c9a11eb9ae7561fa8ede4facb2e5372d
Jhonnyllucius <lucianofloripa@outlook.com>
2025-06-29 23:31:04 -0300

    refactor: tornar propriedades da classe Account imutáveis e melhorar a legibilidade do código

--- lib/accounts/model/account_model.dart
+++ lib/accounts/model/account_model.dart
@@ -1,12 +1,13 @@
+// Salve este conteúdo em: lib/accounts/model/account_model.dart
 import 'package:economize/accounts/enum/account_type.dart';
 
 class Account {
-  int? id;
-  String name;
-  AccountType type;
+  final int? id; // O ID pode ser nulo ANTES de ser salvo no banco
+  final String name;
+  final AccountType type;
   double balance;
-  int icon;
-  String currency;
+  final int icon;
+  final String currency;
 
   Account({
     this.id,
@@ -17,11 +18,31 @@ class Account {
     this.currency = 'BRL',
   });
 
+  // Método para criar uma cópia (boa prática)
+  Account copyWith({
+    int? id,
+    String? name,
+    AccountType? type,
+    double? balance,
+    int? icon,
+    String? currency,
+  }) {
+    return Account(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      type: type ?? this.type,
+      balance: balance ?? this.balance,
+      icon: icon ?? this.icon,
+      currency: currency ?? this.currency,
+    );
+  }
+
+  // Métodos para conversão, essenciais para o DAO
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'name': name,
-      'type': type.index,
+      'type': type.index, // Armazena o enum como um inteiro
       'balance': balance,
       'icon': icon,
       'currency': currency,
@@ -32,7 +53,8 @@ class Account {
     return Account(
       id: map['id'],
       name: map['name'],
-      type: AccountType.values[map['type']],
+      type: AccountType
+          .values[map['type']], // Converte o inteiro de volta para enum
       balance: map['balance'],
       icon: map['icon'],
       currency: map['currency'],
