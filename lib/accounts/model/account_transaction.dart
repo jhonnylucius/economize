61fc57ce3ddfa88cbfd15d881e63d7da6344a8c2
Jhonnyllucius <lucianofloripa@outlook.com>
2025-06-29 23:31:08 -0300

    feat: atualizar tipos de conta para int e melhorar a legibilidade do código

--- lib/accounts/model/account_transaction.dart
+++ lib/accounts/model/account_transaction.dart
@@ -1,13 +1,15 @@
-enum AccountTransactionType { receita, despesa, transferencia }
+// Em lib/accounts/model/account_transaction.dart
+
+enum AccountTransactionType { COST, REVENUE, TRANSFER_IN, TRANSFER_OUT }
 
 class AccountTransaction {
   final String id;
-  final String accountId;
+  final int accountId; // <-- MUDAR PARA int
   final double value;
   final DateTime date;
   final AccountTransactionType type;
   final String? description;
-  final String? relatedAccountId; // Para transferências
+  final int? relatedAccountId; // <-- MUDAR PARA int?
 
   AccountTransaction({
     required this.id,
@@ -18,4 +20,26 @@ class AccountTransaction {
     this.description,
     this.relatedAccountId,
   });
+
+  // Adicionar métodos toMap e fromMap para facilitar a vida no DAO
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'accountId': accountId,
+        'value': value,
+        'date': date.toIso8601String(),
+        'type': type.index,
+        'description': description,
+        'relatedAccountId': relatedAccountId,
+      };
+
+  factory AccountTransaction.fromMap(Map<String, dynamic> map) =>
+      AccountTransaction(
+        id: map['id'],
+        accountId: map['accountId'],
+        value: map['value'],
+        date: DateTime.parse(map['date']),
+        type: AccountTransactionType.values[map['type']],
+        description: map['description'],
+        relatedAccountId: map['relatedAccountId'],
+      );
 }
